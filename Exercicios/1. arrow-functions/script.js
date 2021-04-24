66e24e90476a520a76041876b76462703474f28b
Samdrade <samdrades2@gmail.com>
2021-04-24 17:05:04 -0300

    exercicios de classe

--- Exercicios/1. arrow-functions/script.js
+++ Exercicios/1. arrow-functions/script.js
@@ -1,5 +1,29 @@
-const result = myFunction(4, 3);
+// const result = myFunction(4, 3);
 
 function myFunction(a, b) {
   return a * b;
 }
+
+const iza = {
+  nome: 'Iza',
+  idade: 1,
+  casdrado:true,
+  cor: ['branco', 'preto'],
+  caracteristicas: ['fofinha', 'sociável'],
+}
+
+const beyonce = {
+      nome: 'Beoynce',
+      idade: 1,
+      casdrado:true,
+      cor: ['branco', 'marrom', 'dourado', 'preto'],
+      caracteristicas: ['brincalhão', 'dengoso'],
+  }
+
+  console.log(beyonce.caracteristicas);
+
+  //console.log(iza["nome"]);
+
+  //console.log(iza.nome, beyonce.nome);
+
+ // dot notation console.log(iza["caracteristicas"][0], beyonce["cor"]);
--- Exercicios/2. for/script.js
+++ Exercicios/2. for/script.js
@@ -1,1 +1,5 @@
 const listagemDeFrutas = ["Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+
+for (let fruta = 0; fruta < listagemDeFrutas.length; fruta++) {
+    console.log(listagemDeFrutas[fruta]);
+}
