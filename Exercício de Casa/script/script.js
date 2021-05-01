b82f15c643135ddbddbb03448ef099d06409d6c4
Ester Lourenço <elolourenco@outlook.com>
2021-04-30 21:58:52 -0300

    continuação de projeto

--- Exercício de Casa/script/script.js
+++ Exercício de Casa/script/script.js
@@ -52,4 +52,78 @@ let books = [
         author: "Glenn Block, et al.",
         published: "2014-04-07T00:00:00.000Z",
     }
-]
+] 
+
+function addLivros() {
+ 
+    books.map((book) => {
+        const parsePages = !book.pages ? "-" : book.pages;
+        const parseData = new Date(book.published);
+        const data = parseData.setDate(parseData.getDate() + 1);
+        const dataFormatada = parseData.toLocaleDateString('pt-br');
+ 
+        document.getElementById('corpoTabela').innerHTML +=
+            `
+        <tr >
+        <td> ${book.title} </td>
+        <td> ${book.author}</td>
+        <td> ${book.isbn}</td>
+        <td> ${book.data}</td>
+        <td> ${dataFormatada}</td>
+        <td> ${parsePages}</td>
+        <td><button class="delete"></button></td>
+        </tr>
+        `
+    })
+}
+                                                                                                                      
+
+addLivros();
+ 
+ 
+ function exibirDados(event) {
+    event.preventDefault();
+ 
+    let pegaTitle = document.getElementById('title').value;
+    let pegaAuthor = document.getElementById('author').value;
+    let pegaIsbn = document.getElementById('isbn').value;
+    let pegaDate = document.getElementById('date').value;
+    let pegaPages = document.getElementById('pages').value;
+ 
+  
+    if (pegaTitle === "" || pegaAuthor === "" || pegaIsbn === "" || pegaDate === "" || pegaPages === "") {
+        alert("Campo obrigatório não preenchido")
+    } else {
+        books.push({
+            title: pegaTitle,
+            author: pegaAuthor,
+            isbn: pegaIsbn,
+            published: pegaDate,
+            pages: pegaPages,
+        });
+        limparCampo();
+        addLivros();
+    }
+ 
+}
+ 
+function limparCampo() {
+ 
+    document.getElementById('titulo').value = "";
+    document.getElementById('subTitulo').value = "";
+    document.getElementById('isbn').value = "";
+    document.getElementById('autor').value = "";
+    document.getElementById('paginas').value = "";
+    document.getElementById('dataDePublicacao').value = "";
+    document.getElementById('dataDeInsercao').value = "";
+ 
+}
+ 
+document.getElementById('corpoTabela').addEventListener('click', function removerLinha(event) {
+    if (event.target.className === "delete") {
+        let botao = event.target.parentElement;
+        botao.parentElement.remove();
+    }
+});
+
+                                                                                                                      
