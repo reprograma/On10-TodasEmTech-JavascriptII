c5a101e63137754822bac941c11832248383bc8a
˜Gabrielle <biela.a09@gmail.com>
2021-05-07 19:33:50 -0300

    Adicionando o exercicio 01

--- Exercicios/1. arrow-functions/script.js
+++ Exercicios/1. arrow-functions/script.js
@@ -1,5 +1,10 @@
-const result = myFunction(4, 3);
+// const result = myFunction(4, 3);
 
-function myFunction(a, b) {
-  return a * b;
-}
+const pessoa = {
+  nome: "Iza",
+  idade: 15,
+  casdrado: true,
+  cor: ["branco", "preto"],
+  caracteristicas: ["fofinha", "sociável"],
+  maiordeIdade: false,
+};
