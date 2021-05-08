86da45020083330d6cd37505c74595931f5ec498
Gabrielle Oliveira <biela.a09@gmail.com>
2021-05-07 23:10:54 -0300

    Adicionando exercício 04.

--- Exercicios/4. array.push()/script.js
+++ Exercicios/4. array.push()/script.js
@@ -1,1 +1,14 @@
-const listagemDeFrutas = [ "Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+const listagemDeFrutas = [
+  "Uva",
+  "Banana",
+  "Manga",
+  "Cajá",
+  "Pinha",
+  "Maçã",
+  "Melão",
+];
+listagemDeFrutas.push("Morango", "Tomate");
+
+listagemDeFrutas.push("Açaí", "Cupuaçu");
+
+console.log(listagemDeFrutas);
