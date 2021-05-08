62b40e1a815cacc7baddcb544375167c61db6313
˜Gabrielle <biela.a09@gmail.com>
2021-05-07 23:02:14 -0300

    Adicionando o exercicio 02

--- Exercicios/2. for/script.js
+++ Exercicios/2. for/script.js
@@ -1,1 +1,12 @@
-const listagemDeFrutas = ["Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+const listagemDeFrutas = [
+  "Uva",
+  "Banana",
+  "Manga",
+  "Cajá",
+  "Pinha",
+  "Maçã",
+  "Melão",
+];
+for (let index = 0; index < listagemDeFrutas.length; index++) {
+  console.log(listagemDeFrutas[index]);
+}
