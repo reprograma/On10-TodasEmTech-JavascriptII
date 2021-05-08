5236d34f60d0fca99da7867f04a63128ef8aec62
Gabrielle Oliveira <biela.a09@gmail.com>
2021-05-07 23:26:58 -0300

    Adicionando exercício 09.

--- Exercicios/9. array.splice()/script.js
+++ Exercicios/9. array.splice()/script.js
@@ -1,9 +1,36 @@
-const countryList = [ "Argentina","Armenia","Australia","Azerbaijan","Bahamas","Brazil","Burkina Faso", "Costa Rica","Mauritania","St Vincent","Uganda","United Arab Emirates","Uruguay","Uzbekistan","Venezuela"];
+const countryList = [
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Azerbaijan",
+  "Bahamas",
+  "Brazil",
+  "Burkina Faso",
+  "Costa Rica",
+  "Mauritania",
+  "St Vincent",
+  "Uganda",
+  "United Arab Emirates",
+  "Uruguay",
+  "Uzbekistan",
+  "Venezuela",
+];
+
+countryList.splice(2, 1);
+console.log(countryList);
+
 const usuarios = [
-    {user:234, name: 'Marcia', idade:40 },
-    {user:235, name: 'Lorena', idade:20 },
-    {user:236, name: 'Patricia', idade:24 },
-    {user:237, name: 'Mariana', idade:15 },
-    {user:238, name: 'Isis', idade:34 },
-    {user:239, name: 'Pietra', idade:23 }
-]
+  { user: 234, name: "Marcia", idade: 40 },
+  { user: 235, name: "Lorena", idade: 20 },
+  { user: 236, name: "Patricia", idade: 24 },
+  { user: 237, name: "Mariana", idade: 15 },
+  { user: 238, name: "Isis", idade: 34 },
+  { user: 239, name: "Pietra", idade: 23 },
+];
+usuarios.splice(0, 1, {
+  user: 233,
+  name: "Juliane",
+  idade: 26,
+});
+
+console.log(usuarios);
