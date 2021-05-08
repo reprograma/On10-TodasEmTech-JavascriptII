c57319263eb3d2ff74a6fcc0454ed4a4e54dff06
Gabrielle Oliveira <biela.a09@gmail.com>
2021-05-07 23:11:14 -0300

    Adicionando exercício 06.

--- Exercicios/6. array.shift()/script.js
+++ Exercicios/6. array.shift()/script.js
@@ -1,10 +1,23 @@
-const listagemDeFrutas = [ "Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+const listagemDeFrutas = [
+  "Uva",
+  "Banana",
+  "Manga",
+  "Cajá",
+  "Pinha",
+  "Maçã",
+  "Melão",
+];
 
 const usuarios = [
-    {user:234, name: 'Marcia', idade:40 },
-    {user:235, name: 'Lorena', idade:20 },
-    {user:236, name: 'Patricia', idade:24 },
-    {user:237, name: 'Mariana', idade:15 },
-    {user:238, name: 'Isis', idade:34 },
-    {user:239, name: 'Pietra', idade:23 }
-]
+  { user: 234, name: "Marcia", idade: 40 },
+  { user: 235, name: "Lorena", idade: 20 },
+  { user: 236, name: "Patricia", idade: 24 },
+  { user: 237, name: "Mariana", idade: 15 },
+  { user: 238, name: "Isis", idade: 34 },
+  { user: 239, name: "Pietra", idade: 23 },
+];
+
+listagemDeFrutas.shift();
+
+usuarios.shift();
+console.log(usuarios);
